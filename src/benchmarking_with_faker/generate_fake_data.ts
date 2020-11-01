32c355ffb7a9ef979ded5bb649fccdc58848ef43
Peter 'Pita' Martischka <petermartischka@gmail.com>
2020-11-01 12:18:09 +0100

    fake data benchmark infra

---
+++ src/benchmarking_with_faker/generate_fake_data.ts
@@ -0,0 +1,89 @@
+import faker from 'faker';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const LOCALES = [
+  'az',
+  'ar',
+  'cz',
+  'de',
+  'de_AT',
+  'de_CH',
+  'en',
+  'en_AU',
+  'en_AU_ocker',
+  'en_BORK',
+  'en_CA',
+  'en_GB',
+  'en_IE',
+  'en_IND',
+  'en_US',
+  'en_ZA',
+  'es',
+  'es_MX',
+  'fa',
+  'fi',
+  'fr',
+  'fr_CA',
+  'fr_CH',
+  'ge',
+  'id_ID',
+  'it',
+  'ja',
+  'ko',
+  'nb_NO',
+  'nep',
+  'nl',
+  'nl_BE',
+  'pl',
+  'pt_BR',
+  'pt_PT',
+  'ro',
+  'ru',
+  'sk',
+  'sv',
+  'tr',
+  'uk',
+  'vi',
+  'zh_CN',
+  'zh_TW',
+];
+
+const configs = [
+  {
+    filename: 'emails',
+    fakerFunction: faker.internet.email,
+  },
+  {
+    filename: 'urls',
+    fakerFunction: faker.internet.url,
+  },
+  {
+    filename: 'ip_v4',
+    fakerFunction: faker.internet.ip,
+  },
+  {
+    filename: 'ip_v6',
+    fakerFunction: faker.internet.ipv6,
+  },
+];
+
+configs.forEach(({ filename, fakerFunction }) => {
+  faker.seed(93475);
+  const values: string[] = [];
+  const TOTAL = 100000;
+  const TOTAL_PER_LOCALE = Math.floor(TOTAL / LOCALES.length);
+
+  for (let i = 0; i < TOTAL_PER_LOCALE; i++) {
+    LOCALES.forEach((locale) => {
+      faker.locale = locale;
+      values.push(fakerFunction());
+    });
+  }
+
+  fs.writeFileSync(
+    path.resolve(__dirname, `fake_data/${filename}.json`),
+    JSON.stringify(values, null, 2),
+    'utf8',
+  );
+});
