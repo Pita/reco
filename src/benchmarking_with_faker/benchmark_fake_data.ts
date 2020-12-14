54b9d8fc66daa615aa1382fb883062b5bee48370
Peter 'Pita' Martischka <petermartischka@gmail.com>
2020-12-14 14:44:03 +0100

    Improved faker benchmark

--- src/benchmarking_with_faker/benchmark_fake_data.ts
+++ src/benchmarking_with_faker/benchmark_fake_data.ts
@@ -31,57 +31,70 @@ function runFakeDataBenchmark(file: string, regex: string) {
     console.log('originalRegex', originalRegex);
 
     const startOriginalRegex = Date.now();
-    let sum = 0;
+    let originalRegexCheckSum = 0;
     testData.forEach((testValue: string) => {
       const match = originalRegex.exec(testValue);
       if (match) {
         for (let i = 0; i < match.length; i++) {
-          sum += typeof match[i] === 'string' ? match[i].length : 0;
+          originalRegexCheckSum +=
+            typeof match[i] === 'string' ? match[i].length : 0;
         }
       }
     });
     const durationOriginalRegex = Date.now() - startOriginalRegex;
-    console.log('Native checksum:', sum);
     console.log(`Native duration: ${durationOriginalRegex}ms`);
-    return durationOriginalRegex;
+    return { durationOriginalRegex, originalRegexCheckSum };
   }
 
   function benchmarkGeneratedRegex(testData: string[]) {
     const generatedRegex = getGeneratedRegexCode();
 
     const startGeneratedRegex = Date.now();
-    let sum = 0;
+    let generatedRegexCheckSum = 0;
     testData.forEach((testValue: string) => {
       const result = generatedRegex(testValue);
-      if (result && result.match) {
-        for (let i = 0; i < result.match.length; i++) {
-          sum +=
-            typeof result.match[i] === 'string' ? result.match[i].length : 0;
+      if (result) {
+        for (let i = 0; i < result.matches.length; i++) {
+          generatedRegexCheckSum +=
+            typeof result.matches[i] === 'string'
+              ? result.matches[i].length
+              : 0;
         }
       }
     });
     const durationGeneratedRegex = Date.now() - startGeneratedRegex;
-    console.log('Generated checksum:', sum);
     console.log(`Generated duration: ${durationGeneratedRegex}ms`);
-    return durationGeneratedRegex;
+    return { durationGeneratedRegex, generatedRegexCheckSum };
   }
 
   console.log(regex.toUpperCase());
   const testData = getTestData();
-  const nativeDuration = benchmarkNativeRegex(testData);
-  const generatedDuration = benchmarkGeneratedRegex(testData);
+  const { durationOriginalRegex, originalRegexCheckSum } = benchmarkNativeRegex(
+    testData,
+  );
+  const {
+    durationGeneratedRegex,
+    generatedRegexCheckSum,
+  } = benchmarkGeneratedRegex(testData);
 
-  const factor = generatedDuration / nativeDuration;
+  const factor = durationGeneratedRegex / durationOriginalRegex;
+  if (generatedRegexCheckSum !== originalRegexCheckSum) {
+    console.error(
+      'Different checksums original vs generated!!!',
+      generatedRegexCheckSum,
+      originalRegexCheckSum,
+    );
+  }
   console.log(`FACTOR: ${factor.toFixed(1)}`, '\n');
   return factor;
 }
 
-// runFakeDataBenchmark('emails', 'common_regex/email1');
-// runFakeDataBenchmark('emails', 'common_regex/email2');
-// runFakeDataBenchmark('emails', 'common_regex/email3');
-// runFakeDataBenchmark('ip_v4', 'common_regex/ipv4');
-// runFakeDataBenchmark('ip_v6', 'common_regex/ipv6');
-// runFakeDataBenchmark('ip_v4', 'common_regex/ipv4_and_ipv6');
-// runFakeDataBenchmark('ip_v6', 'common_regex/ipv4_and_ipv6');
+runFakeDataBenchmark('emails', 'common_regex/email1');
+runFakeDataBenchmark('emails', 'common_regex/email2');
+runFakeDataBenchmark('emails', 'common_regex/email3');
+runFakeDataBenchmark('ip_v4', 'common_regex/ipv4');
+runFakeDataBenchmark('ip_v6', 'common_regex/ipv6');
+runFakeDataBenchmark('ip_v4', 'common_regex/ipv4_and_ipv6');
+runFakeDataBenchmark('ip_v6', 'common_regex/ipv4_and_ipv6');
 runFakeDataBenchmark('urls', 'common_regex/url_http');
 runFakeDataBenchmark('urls', 'common_regex/url_protocol_optional');
