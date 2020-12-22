019090403621940165a202aa0d4688681e065c36
Peter 'Pita' Martischka <petermartischka@gmail.com>
2020-12-22 23:04:07 +0100

    Expose API

---
+++ src/api.ts
@@ -0,0 +1,3 @@
+export { genTemplateValues } from './generator/generator';
+export { template } from './generator/templates/mainTemplate';
+export { transformCode } from './generator/transformCode';
