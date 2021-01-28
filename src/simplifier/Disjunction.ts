2ba8b829bb3c5f0b537917c150205ce10d7e3d8d
Peter 'Pita' Martischka <petermartischka@gmail.com>
2021-01-28 17:53:31 +0100

    Remove common elements from the end as well

--- src/simplifier/Disjunction.ts
+++ src/simplifier/Disjunction.ts
@@ -2,6 +2,51 @@ import { AST } from 'regexpp';
 import { handleAlternative } from './Alternative';
 import { SimplifierHandler } from './types';
 
+const removeFromSide = (
+  alternatives: AST.Alternative[],
+  side: 'start' | 'end',
+) => {
+  let removed = '';
+
+  while (true) {
+    const needleIndex =
+      side === 'start' ? 0 : alternatives[0].elements.length - 1;
+    const needle = alternatives[0].elements[needleIndex];
+
+    if (!needle || needle.type === 'CapturingGroup') {
+      break;
+    }
+
+    const allAlternativesHaveNeedle = alternatives.every((alternative) => {
+      const elementIndex =
+        side === 'start' ? 0 : alternative.elements.length - 1;
+      const element = alternative.elements[elementIndex];
+      if (!element) {
+        return false;
+      }
+
+      return element.raw === needle.raw && element.type === needle.type;
+    });
+
+    if (allAlternativesHaveNeedle) {
+      alternatives.forEach((alternative) =>
+        side === 'start'
+          ? alternative.elements.shift()
+          : alternative.elements.pop(),
+      );
+
+      removed =
+        side === 'start'
+          ? `${removed}${needle.raw}`
+          : `${needle.raw}${removed}`;
+    } else {
+      break;
+    }
+  }
+
+  return removed;
+};
+
 export const handleDisjunction: SimplifierHandler<AST.Alternative[]> = (
   alternatives,
   options,
@@ -10,36 +55,15 @@ export const handleDisjunction: SimplifierHandler<AST.Alternative[]> = (
     return handleAlternative(alternatives[0], options);
   }
 
-  let preAlternatives = '';
-
-  while (true) {
-    const needle = alternatives[0].elements[0];
-
-    if (!needle || needle.type === 'CapturingGroup') {
-      break;
-    }
-
-    const allAlternativesHaveNeedle = alternatives.every(
-      (alternative) =>
-        alternative.elements[0].raw === needle.raw &&
-        alternative.elements[0].type === needle.type,
-    );
-
-    if (allAlternativesHaveNeedle) {
-      alternatives.forEach((alternative) => alternative.elements.shift());
-
-      preAlternatives += needle.raw;
-    } else {
-      break;
-    }
-  }
+  const removedFromStart = removeFromSide(alternatives, 'start');
+  const removedFromEnd = removeFromSide(alternatives, 'end');
 
   const alternativesString = alternatives
     .map((alternative) => handleAlternative(alternative, options))
     .join('|');
 
-  if (preAlternatives) {
-    return `${preAlternatives}(?:${alternativesString})`;
+  if (removedFromStart !== '' || removedFromEnd !== '') {
+    return `${removedFromStart}(?:${alternativesString})${removedFromEnd}`;
   } else {
     return alternativesString;
   }
--- src/simplifier/simplifyRegex.test.ts
+++ src/simplifier/simplifyRegex.test.ts
@@ -3,9 +3,16 @@ import { simplifyRegex } from './simplifyRegex';
 
 describe('simplifyRegex', () => {
   test('removes elements from the beginning of each alternative', () => {
-    const regex = new RegExpParser().parseLiteral('/abce|abde/i');
+    const regex = new RegExpParser().parseLiteral('/abce|abdf/i');
     const simplified = simplifyRegex(regex);
 
-    expect(simplified).toEqual('/ab(?:ce|de)/i');
+    expect(simplified).toEqual('/ab(?:ce|df)/i');
+  });
+
+  test('removes elements from the end of each alternative', () => {
+    const regex = new RegExpParser().parseLiteral('/abef|cdef/i');
+    const simplified = simplifyRegex(regex);
+
+    expect(simplified).toEqual('/(?:ab|cd)ef/i');
   });
 });
